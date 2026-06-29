29d1c4778eebcc64e39bad8bd8f625cf5c949295
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2026-06-29 16:11:54 -0700

    refactor: rename project to contrail

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: [
+    "deck.gl",
+    "@deck.gl/core",
+    "@deck.gl/layers",
+    "@deck.gl/react",
+    "@deck.gl/geo-layers",
+    "@deck.gl/aggregation-layers",
+    "@luma.gl/core",
+    "@luma.gl/webgl",
+    "@luma.gl/engine",
+    "@luma.gl/shadertools",
+    "@loaders.gl/core",
+    "@loaders.gl/images",
+    "react-map-gl",
+    "maplibre-gl",
+  ],
 };
 
 export default nextConfig;
